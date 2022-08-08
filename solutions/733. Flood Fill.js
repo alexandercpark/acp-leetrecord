600d14a5d0e0df09e7e771c0b3a4328115058e8d
alexandercpark <alexandercpark@gmail.com>
2022-08-07 17:20:37 -0700

    104 ms

---
+++ solutions/733. Flood Fill.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+​
+const DIRECTIONS = [
+    [-1,0],
+    [1,0],
+    [0,-1],
+    [0,1]
+];
+​
+var floodFill = function(image, sr, sc, color) {
+    // really ashsmed of this one, i need to revisit this
+    let startingColour = image[sr][sc];
+    if(startingColour === color) {
+        return image;
+    }
+    let nodes = [[sr,sc]];
+    let closedList = new Map();
+    
+    while(nodes.length > 0) {
+        let [x,y] = nodes.pop();
+        if(x < 0 || y < 0) {
+            continue;
+        }
+        if(x > image.length - 1 || y > image[x].length - 1) {
