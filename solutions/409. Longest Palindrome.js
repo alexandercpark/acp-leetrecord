344f9bdc2912b5e5b99a23c2619640a17a6869e3
alexandercpark <alexandercpark@gmail.com>
2022-08-07 19:01:25 -0700

    81 ms

---
+++ solutions/409. Longest Palindrome.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let chars = {};
+    
+    for(let char of s) {
+        if(!chars[char]) {
+            chars[char] = 0;
+        }
+        
+        chars[char]++;
+    }
+    
+    let soln = 0;
+    let hasOdd = false;
+    for(let key in chars) {
+        let value = chars[key];
+        
+        if(value % 2 === 0) {
+            soln += value;
+        } else {
+            soln += value - 1;
+            hasOdd = true;
+        }
+    }
+    
+    return hasOdd ? evens + 1 : evens;
+};
