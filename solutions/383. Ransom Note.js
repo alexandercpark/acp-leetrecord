98d46710b3c24eeef5b9e0a7e1e7629814711658
alexandercpark <alexandercpark@gmail.com>
2022-08-07 18:45:23 -0700

    96 ms

--- solutions/383. Ransom Note.js
+++ solutions/383. Ransom Note.js
@@ -4,16 +4,16 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    let dictionary = new Array(26).fill(0);
+    let letters = new Array(26).fill(0);
     let top = 'z'.charCodeAt(0);
     
     for(let char of ransomNote) {
-        dictionary[top - char.charCodeAt(0)]++;
+        letters[top - char.charCodeAt(0)]++;
     }
     
     for(let char of magazine) {
-        dictionary[top - char.charCodeAt(0)]--;
+        letters[top - char.charCodeAt(0)]--;
     }
     
-    return !dictionary.some(letter => letter > 0);
+    return !letters.some(letter => letter > 0);
 };
